ed317fb75642dff1e0cdedf38962cbb6e5559e4d
Gabriel <gabriel@garciaconde.com.mx>
2020-02-04 16:43:57 -0500

    menu lateral

--- resources/views/blank.blade.php
+++ resources/views/blank.blade.php
@@ -6,55 +6,39 @@ Admin AEPSA Riviera
 
 @section('contenido')
 <div class="container-fluid">
-            <div class="block-header">
-                <div class="row">
-                    <div class="col-lg-5 col-md-8 col-sm-12">                        
-                        <h2><a href="javascript:void(0);" class="btn btn-xs btn-link btn-toggle-fullwidth"><i class="fa fa-arrow-left"></i></a> Page Blank</h2>
-                        <ul class="breadcrumb">
-                            <li class="breadcrumb-item"><a href="index.html"><i class="icon-home"></i></a></li>                            
-                            <li class="breadcrumb-item">Pages</li>
-                            <li class="breadcrumb-item active">Page Blank</li>
-                        </ul>
-                    </div>            
-                    <div class="col-lg-7 col-md-4 col-sm-12 text-right">
-                        <div class="inlineblock text-center m-r-15 m-l-15 hidden-sm">
-                            <div class="sparkline text-left" data-type="line" data-width="8em" data-height="20px" data-line-Width="1" data-line-Color="#00c5dc"
-                                data-fill-Color="transparent">3,5,1,6,5,4,8,3</div>
-                            <span>Visitors</span>
-                        </div>
-                        <div class="inlineblock text-center m-r-15 m-l-15 hidden-sm">
-                            <div class="sparkline text-left" data-type="line" data-width="8em" data-height="20px" data-line-Width="1" data-line-Color="#f4516c"
-                                data-fill-Color="transparent">4,6,3,2,5,6,5,4</div>
-                            <span>Visits</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="row clearfix">
-
-                <div class="col-lg-12 col-md-12">
-                    <div class="card planned_task">
-                        <div class="header">
-                            <h2>Stater Page</h2>
-                            <ul class="header-dropdown">
-                                <li class="dropdown">
-                                    <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"></a>
-                                    <ul class="dropdown-menu dropdown-menu-right">
-                                        <li><a href="javascript:void(0);">Action</a></li>
-                                        <li><a href="javascript:void(0);">Another Action</a></li>
-                                        <li><a href="javascript:void(0);">Something else</a></li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="body">
-                            <h4>Stater Page</h4>
-                        </div>
-                    </div>
-                </div>
-
+    <div class="block-header">
+        <div class="row">
+            <div class="col-lg-5 col-md-8 col-sm-12">                        
+                <ul class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="index.html"><i class="icon-home"></i></a></li>                            
+                    <li class="breadcrumb-item">Pages</li>
+                    <li class="breadcrumb-item active">Page Blank</li>
+                </ul>
             </div>
         </div>
+    </div>
+    <div class="row clearfix">
+        <div class="col-lg-12 col-md-12">
+            <div class="card planned_task">
+                <div class="header">
+                    <h2>Stater Page</h2>
+                    <ul class="header-dropdown">
+                        <li class="dropdown">
+                            <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"></a>
+                            <ul class="dropdown-menu dropdown-menu-right">
+                                <li><a href="javascript:void(0);">Action</a></li>
+                                <li><a href="javascript:void(0);">Another Action</a></li>
+                                <li><a href="javascript:void(0);">Something else</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+                <div class="body">
+                    <h4>Stater Page</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 @stop
