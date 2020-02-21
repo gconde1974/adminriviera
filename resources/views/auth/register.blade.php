fa9ebb16f83a4048e1bc3f404161574099c3b5ff
Elizabeth <elizabeth.dev27@gmail.com>
2020-02-20 20:55:09 -0500

    register
    --

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,7 @@
-@extends('layouts.app')
+@extends('layout.loginTemplate')
 
-@section('content')
+
+@section('contenido')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
